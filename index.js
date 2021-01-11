2520cf4ada5e26c3cc3b42b2b680510954dbadd8
danajackson2 <danajackson2@gmail.com>
2021-01-11 15:56:34 -0800

    VICTORY IS MINE

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Write your code here!
+main.remove()
+let newHeader = document.createElement('h1')
+newHeader.id = "victory"
+document.body.appendChild(newHeader)
+newHeader.innerHTML = "YOUR-NAME is the champion"
+
+// let element = document.createElement('div') 
+// document.body.appendChild(element) 
+// let ul = document.createElement('ul')
+
+// for (let i = 0; i < 3; i++) {
+//   let li = document.createElement('li')
+//   li.innerHTML = (i + 1).toString()
+//   ul.appendChild(li)
+// }
+// element.appendChild(ul) 
+
+
+// let header = document.getElementById("header");
+// header.innerHTML = "<h1>Poodles!</h1><h3>An Essay into the Pom-Pom as Aesthetic Reconfiguration of the Other from a post-Frankfurt School Appropriationist Perspective</h3><p><em>By: Byron Q. Poodle, Esq., BA.</em></p>"; 
+// element.style.backgroundColor = '#27647B'; 
+// element.style.textAlign = 'center';
+// ul.style.textAlign = 'left'; 
+// element.className = "dog cat";
+// element.classList.remove("dog")
+// element.classList.add("horse")
+// ul.removeChild(ul.querySelector('li:nth-child(2)'))
+//ul.remove()
